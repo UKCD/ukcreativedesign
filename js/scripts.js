2c17e2a4be0297f3b3ac28dbb8a8b6696acc26fe
uk-creative-design <info@uk-creative-design.co.uk>
2016-09-07 13:16:14 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+$(window).load(function() {
+    // Animate loader off screen
+    $(".se-pre-con").fadeOut("slow");
+
+    //hide menu2 icon on page load
+    $('#menu2').hide();
+
+});
+
+$(document).ready(function() {
+    $('#quote-carousel').carousel({
+        pause: true, interval: 10000,
+    });
+
+
+    var targetOffset = $("#separator").offset().top;
+
+    var $w = $(window).scroll(function(){
+        if ( $w.scrollTop() > targetOffset ) {
+            $('#menu1').hide();
+            $('#menu2').show();
+        } else {
+            $('#menu1').show();
+            $('#menu2').hide();
+        }
+    });
+
+    $( "#scroll-btn" ).click(function() {
+        alert('pressed');
+        $('#menu1').hide();
+        $('#menu2').show();
+    });
+
+
+
+
+
+});<!--end document ready function-->
