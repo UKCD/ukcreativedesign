158efd3f32288dcd2f1e52036087d911531ef764
romaingaget <romaingaget@gmail.com>
2016-09-28 12:51:02 +0100

    changed jquery version

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(window).load(function() {
+$(window).on('load',function() {
     // Animate loader off screen
     $(".se-pre-con").fadeOut("slow");
 
@@ -6,8 +6,8 @@ $(window).load(function() {
     $('#menu2').hide();
 
 });
+//
 
-$(document).ready(function() {
     $('#quote-carousel').carousel({
         pause: true, interval: 10000,
     });
@@ -25,15 +25,27 @@ $(document).ready(function() {
         }
     });
 
-    $( "#scroll-btn" ).click(function() {
-        alert('pressed');
-        $('#menu1').hide();
-        $('#menu2').show();
-    });
+    // $( "#scroll-btn" ).click(function() {
+    //     alert('pressed');
+    //     $('#menu1').hide();
+    //     $('#menu2').show();
+    // });
 
+    $("#scroll-btn").click(function(event){
+            //prevent the default action for the click event
+            event.preventDefault();
 
+            //get the full url - like mysitecom/index.htm#home
+            var full_url = this.href;
 
+            //split the url by # and get the anchor target name - home in mysitecom/index.htm#home
+            var parts = full_url.split("#");
+            var trgt = parts[1];
 
+            //get the top offset of the target anchor
+            var target_offset = $("#"+trgt).offset();
+            var target_top = target_offset.top;
 
-
-});<!--end document ready function-->
+            //goto that anchor by setting the body scroll top to anchor top
+            $('html, body').animate({scrollTop:target_top}, 1500, 'easeInSine');
+        });
