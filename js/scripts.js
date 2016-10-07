20776f348d3160490c29888ceb0708a8cea08f7c
romaingaget <romaingaget@gmail.com>
2016-10-07 14:11:47 +0100

    added smooth scrolling and added link to the top button view work

--- js/scripts.js
+++ js/scripts.js
@@ -48,4 +48,24 @@ $(document).ready(function() {
           $(".tooltip").css("opacity", "1");
         });
 
+
+
+        $(function() {
+          $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+              if (target.length) {
+                $('html, body').animate({
+                  scrollTop: target.offset().top
+                }, 1000);
+                return false;
+              }
+            }
+          });
+});
+
+
+
+
     }); //close document ready
